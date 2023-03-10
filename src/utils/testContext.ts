fb1f34ba0ad38c62576791b786dc287d93062f9a
SOMON SOUM <somon.soum9@gmail.com>
2023-03-11 06:18:33 +0700

    fix

--- src/utils/testContext.ts
+++ src/utils/testContext.ts
@@ -1,19 +1,32 @@
-import { ApolloServer, BaseContext } from '@apollo/server'
+import { ApolloServer, BaseContext } from '@apollo/server';
 import { schema } from '~/schema';
 
-
 export const testServer = async () => {
-    const server = new ApolloServer({
-        schema: schema
-    });
+  const server = new ApolloServer({
+    schema: schema,
+  });
 
-    const query = async<T extends BaseContext>(query: string, variables?: any, contextValue?: T) => {
-        return {} = server.executeOperation({ query: `query ${query}`, variables }, { contextValue })
-    }
+  const query = async <T extends BaseContext>(
+    query: string,
+    variables?: any,
+    contextValue?: T,
+  ) => {
+    return ({} = server.executeOperation(
+      { query: `query ${query}`, variables },
+      { contextValue },
+    ));
+  };
 
-    const mutate = async<T extends BaseContext> (mutation: string, variables?: any, contextValue?: T) => {
-        return server.executeOperation({ query: `mutation ${mutation}`, variables, }, { contextValue })
-    }
+  const mutate = async <T extends BaseContext>(
+    mutation: string,
+    variables?: any,
+    contextValue?: T,
+  ) => {
+    return server.executeOperation(
+      { query: `mutation ${mutation}`, variables },
+      { contextValue },
+    );
+  };
 
-    return { query, mutate };
-}
+  return { query, mutate };
+};
